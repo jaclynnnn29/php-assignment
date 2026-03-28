e44ef0a38d2573bfa670114208701ea9eeb471f4
jaclynnnn29 <angelinajaclyn00@gmail.com>
2026-03-28 12:56:46 +0800

    Make change on CSS

---
+++ M&H Online Shopping/product/detail.php
@@ -0,0 +1,81 @@
+<?php
+include '../_base.php';
+
+// ----------------------------------------------------------------------------
+
+if (is_post()) {
+    // TODO
+    $id = req('id');
+    $unit = req('unit]');
+    update_cart($id, $unit);
+    redirect();
+}
+
+$id  = req('id');
+$stm = $_db->prepare('SELECT * FROM product WHERE id = ?');
+$stm->execute([$id]);
+$p = $stm->fetch();
+if (!$p) redirect('list.php');
+
+// ----------------------------------------------------------------------------
+
+$_title = 'Product | Detail';
+include '../_head.php';
+?>
+
+<style>
+    #photo {
+        display: block;
+        border: 1px solid #333;
+        width: 200px;
+        height: 200px;
+    }
+</style>
+
+<p>
+    <img src="/products/<?= $p->photo ?>" id="photo">
+</p>
+
+<table class="table detail">
+    <tr>
+        <th>Id</th>
+        <td><?= $p->id ?></td>
+    </tr>
+    <tr>
+        <th>Name</th>
+        <td><?= $p->name ?></td>
+    </tr>
+    <tr>
+        <th>Price</th>
+        <td>RM <?= $p->price ?></td>
+    </tr>
+    <tr>
+        <th>Unit</th>
+        <td>
+            <!-- TODO -->
+             <?php
+             $cart = get_cart();
+             $id = $p->id;
+             $unit = $cart[$p->id] ?? 0;
+             ?>
+            <form method="post">
+                <!-- TODO ✅ -->
+                 <?= html_hidden('id') ?>
+                 <?= html_select('unit', $_units, '') ?>
+                 <?=  $unit ? '✅' : '' ?>
+            </form>
+        </td>
+    </tr>
+</table>
+
+<p>
+    <button data-get="list.php">List</button>
+</p>
+
+<script>
+    // TODO
+    $('select').on('change', e => e.target.form.submit());
+</script>
+
+<?php
+include '../_foot.php';
---
+++ M&H Online Shopping/product/list.php
@@ -0,0 +1,92 @@
+<?php
+include '../_base.php';
+
+// ----------------------------------------------------------------------------
+
+if (is_post()) {
+    // TODO
+    $id = req('id');
+    $unit = req('unit');
+    update_cart($id, $unit);
+    redirect();
+}
+
+$arr = $_db->query('SELECT * FROM product');
+
+// ----------------------------------------------------------------------------
+
+$_title = 'Product | List';
+include '../_head.php';
+?>
+
+<style>
+    #products {
+        display: flex;
+        gap: 10px;
+        flex-wrap: wrap;
+    }
+
+    .product {
+        border: 1px solid #333;
+        width: 200px;
+        height: 200px;
+        position: relative;
+    }
+
+    .product img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+    }
+
+    .product form,
+    .product div {
+        position: absolute;
+        background: #0009;
+        color: #fff;
+        padding: 5px;
+        text-align: center;
+    }
+
+    .product form {
+        inset: 0 0 auto auto;
+    }
+
+    .product div {
+        inset: auto 0 0 0;
+    }
+</style>
+
+<div id="products">
+    <?php foreach ($arr as $p): ?>
+        <!-- TODO -->
+        <?php
+         $cart = get_cart();
+         $id = $p->id;
+         $unit = $cart[$p->id] ?? 0;
+         ?>
+
+        <div class="product">
+            <form method="post">
+                <!-- TODO ✅ -->
+                 <?=  $unit ? '✅' : '' ?>
+                 <?= html_hidden('id') ?>
+                 <?= html_select('unit', $_units, '') ?>
+            </form>
+                
+            <img src="/products/<?= $p->photo ?>"
+                 data-get="/product/detail.php?id=<?= $p->id ?>">
+
+            <div><?= $p->name ?> | RM <?= $p->price ?></div>
+        </div>
+    <?php endforeach ?>
+</div>
+
+<script>
+    // TODO
+    $('select').on('change', e => e.target.form.submit());
+</script>
+
+<?php
+include '../_foot.php';
