2716b29bcb528e526ea02ff5d86b55eb6494a5de
jaclynnnn29 <angelinajaclyn00@gmail.com>
2026-04-14 02:28:49 +0800

    changes

--- M&H Online Shopping/admin/product_delete.php
+++ M&H Online Shopping/admin/product_delete.php
@@ -5,14 +5,15 @@ auth('Admin');
 // 1. Get the product_id from the URL
 $id = req('id');
 
-// 2. Fetch product first to find the photo filename (optional but recommended)
+// 2. Fetch product first to find the photo filename
 $stm = $_db->prepare("SELECT photo FROM product WHERE product_id = ?");
 $stm->execute([$id]);
 $photo = $stm->fetchColumn();
 
-// 3. Delete the photo file from your folder so you don't waste space
+// 3. Delete the photo file from your folder
 if ($photo) {
-    @unlink("../uploads/$photo");
+    // Make sure this path matches where your save_photo() function stores files
+    @unlink("../images/$photo"); 
 }
 
 // 4. Delete the actual record from the product table
@@ -21,7 +22,6 @@ $stm->execute([$id]);
 
 temp('info', "Product $id has been deleted.");
 
-// 5. Redirect back to the Product List (not the user list!)
-if (!$id){
-    redirect('product_list.php');
-}
+// 5. ALWAYS redirect back to the Product List
+redirect('product_list.php'); 
+?>
