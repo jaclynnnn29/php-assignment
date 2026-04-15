775e56795bba4ea13471ff5b50fdbc3ffaff89ca
Ee Xiang <ceexiang@gmail.com>
2026-04-15 19:07:30 +0800

    resolve icon in tab issue

--- M&H Online Shopping/_head.php
+++ M&H Online Shopping/_head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $_title ?? 'Untitled' ?></title>
+    <link rel="icon" href="/images/favicon.png">
     <link rel="stylesheet" href="../css/app.css"> 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
