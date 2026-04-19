b3789582d8ed275da30f44df58d8251884fa4125
jaclynnnn29 <angelinajaclyn00@gmail.com>
2026-04-19 10:00:44 +0800

    trying

--- M&H Online Shopping/admin/order_detail.php
+++ M&H Online Shopping/admin/order_detail.php
@@ -9,7 +9,7 @@ $order_id = req('id');
 if (is_post()) {
     $new_status = post('shipment_status'); 
     // Fix: Updating the correct column 'shipment_status' in 'orders' table
-    $stm = $_db->prepare("UPDATE orders SET shipment_status = ? WHERE order_id = ?");
+    $stm = $_db->prepare("UPDATE orders SET status = ? WHERE order_id = ?");
     $stm->execute([$new_status, $order_id]);
     temp('info', 'Order status updated successfully');
     redirect(); 
